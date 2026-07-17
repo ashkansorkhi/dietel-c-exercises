98e9e6637909071568888f382b2ad8cd2595a609
ashkansorkhi <ashkan0sorkhi@gmail.com>
2026-07-17 17:40:56 +0330

    added delete func to fig.12.1

--- fig.12.1 S .12/main.c
+++ fig.12.1 S .12/main.c
@@ -10,12 +10,16 @@ typedef struct listNode
 typedef listNode *listNodePtr;
 void insert(listNodePtr *,char );
 void printNode(const listNodePtr *);
-
+char delete(listNodePtr *startPtr,char value);
  int main(void){
     listNodePtr startPtr = NULL;
     char character = ' ';
-
-    puts("enter the character");
+    int choice = 0 ;
+    puts("please Enter:\n1.to add new character and\n2.to delete a character");
+    switch (choice)
+    {
+    case 1:
+          puts("enter the character");
     scanf(" %c",&character);
     puts("");
     while (character != '0')
@@ -25,6 +29,14 @@ void printNode(const listNodePtr *);
      scanf(" %c",&character);
      puts("");
     }
+        break;
+    case 2:
+
+        break;
+    default:
+        break;
+    }
+ 
     
     
  }
@@ -71,4 +83,34 @@ void printNode(const listNodePtr *);
         printf("%c --> ", currentPtr->value);
         currentPtr = currentPtr->nextPtr;
     }
+ }
+
+
+ char delete(listNodePtr *startPtr,char value){
+
+    if(value = (*startPtr)->value){
+        listNodePtr tempNode = *startPtr;
+        *startPtr = (*startPtr)->nextPtr;
+        free(tempNode);
+        return value;
+    }
+
+    else{
+        listNodePtr previousNode = *startPtr;
+        listNodePtr currentNode = (*startPtr)->nextPtr;
+        if(currentNode != NULL && currentNode->value != value){
+            previousNode = currentNode;
+            currentNode = currentNode->nextPtr;
+        }
+
+        if(currentNode !=NULL){
+            listNodePtr tempNode = currentNode;
+            currentNode = currentNode->nextPtr;
+            free(tempNode);
+            return value;
+        }
+
+    }
+    return '\0';
+
  }
