02ddaf1be41516e87658950bb048e76e48f6223d
ashkansorkhi <ashkan0sorkhi@gmail.com>
2026-07-19 15:45:40 +0330

    added binary tree

---
+++ binary trees/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+//structs 
+
+struct binaryTree {
+    
+    struct binaryTree leftSubTree;
+    int data;
+    struct binaryTree rightSubTree;
+    
+};
+
+
+
+int main(void){
+
+    
+
+}
