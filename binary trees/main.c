fa7644993b6b8439b052dafb86d2de2f224236ba
ashkansorkhi <ashkan0sorkhi@gmail.com>
2026-07-20 00:21:31 +0330

    added insertNode to binaryTree

--- binary trees/main.c
+++ binary trees/main.c
@@ -7,16 +7,75 @@
 
 struct binaryTree {
     
-    struct binaryTree leftSubTree;
+    struct binaryTree *leftSubTree;
     int data;
-    struct binaryTree rightSubTree;
+    struct binaryTree *rightSubTree;
     
 };
 
+//typedefs
+typedef struct binaryTree binaryTree ;
+typedef struct binaryTree *binaryTreePtr ;
+
+//prototypes
+void insertNode(int value , binaryTreePtr * parentNode); 
 
 
 int main(void){
 
+int number = 0;
+binaryTreePtr parent = NULL;
+srand(time(NULL));
+
+puts("the numbers are:");
+
+for(int i =1 ; i<=10; i++){
+    number = rand() % 15;
+    printf("%d ",number);
+    insertNode(number,&parent);
+}
     
+
+}
+
+
+// functions 
+
+
+void insertNode(int value , binaryTreePtr * parentNode)
+{
+
+    if(*parentNode == NULL)
+    {
+        binaryTreePtr newPtr = malloc(sizeof(binaryTree));
+        if(newPtr != NULL)
+        {
+          newPtr->data = value;
+          newPtr->leftSubTree = NULL;
+          newPtr->rightSubTree = NULL;
+          * parentNode = newPtr;
+        }
+        else
+        {
+            puts("not enough space");
+        }
+
+    }
+    else if(value < (*parentNode)->data)
+    {
+        insertNode(value, &(*parentNode)->leftSubTree);
+    }
+
+
+    else if(value > (*parentNode)->data)
+    {
+        insertNode(value, &(*parentNode)->rightSubTree);
+    }
+    else
+    {
+        printf("%s","dub");
+    }
+
+
 
 }
