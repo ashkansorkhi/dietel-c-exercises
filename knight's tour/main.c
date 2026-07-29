dde87158e7f7cc48088d4570f4cc15d792699d6b
ashkansorkhi <ashkan0sorkhi@gmail.com>
2026-07-29 05:01:40 +0330

    added knight's tour

---
+++ knight's tour/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define COLUMN_SIZE 8
+#define ROW_SIZE 8
+
+
+
+
+
+
+int main(void)
+{
+   const signed char moves[8][2] =
+    {
+        {2,-1},
+        {1,-2},
+        {-1,-2},
+        {-2,-1},
+        {-2,1},
+        {-1,2},
+        {1,2},
+        {2,1}
+    }; 
+
+
+}
